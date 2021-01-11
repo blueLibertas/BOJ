e5414222dacf19b08520a86212ff66b547a833d3
blueLibertas <bluelibertas@yonsei.ac.kr>
2021-01-12 01:48:29 +0900

    Create #2775 부녀회장이 될테야.py
    Initial commit

---
+++ Python/#2775 부녀회장이 될테야.py
@@ -0,0 +1,12 @@
+N = int(input())
+for i in range(N):
+    k = int(input())
+    n = int(input())
+
+    living = [[x for x in range(n+1)]]
+    for floor in range(1, k+1):
+        temp = [1, 1]
+        for num in range(2, n+1):
+            temp.append(sum(living[floor-1][1:num+1]))
+        living.append(temp)
+    print(living[k][n])
