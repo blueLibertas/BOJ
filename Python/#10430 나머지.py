b7a9b4c6a88c4b14fdb5f468ea2d17ff18fe96e8
blueLibertas <bluelibertas@yonsei.ac.kr>
2021-01-15 16:11:30 +0900

    Create #10430 나머지.py

---
+++ Python/#10430 나머지.py
@@ -0,0 +1,5 @@
+# 문제 출처: https://www.acmicpc.net/problem/10430
+# 풀이: 2021.01.15
+
+A, B, C = map(int, input().split())
+print("{}\n{}\n{}\n{}\n".format((A+B)%C, ((A%C) + (B%C))%C, (A*B)%C, ((A%C)*(B%C))%C))
