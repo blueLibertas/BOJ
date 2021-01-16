1a642fb7cd1a87d7675f1f173f2e4a5d6f104f43
blueLibertas <bluelibertas@yonsei.ac.kr>
2021-01-16 19:17:58 +0900

    Create 빠른 IO.py

---
+++ 빠른 IO.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/15552
+# https://www.acmicpc.net/board/view/22716
+
+"""
+Java
+Scanner와 System.out.println (X)
+BufferedReader, BufferedWriter, 마지막에 BufferedWriter.flush (O)
+"""
+
+"""
+Python
+input() (X)
+sys.stdin.readline().rstrip() (O)
+"""
